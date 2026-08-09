8cf6991907b84333c6cef9a0b4fd0769f8791a35
Sachin Kumar <sachinxyz625@gmail.com>
2026-08-09 16:41:40 +0530

    Switch email service from Gmail SMTP to Resend API

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -1,40 +1,23 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        type: "OAuth2",
-        user: process.env.EMAIL_USER,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-    },
-});
-
-
-// Verify the connection configuration
-transporter.verify((error, success) => {
-    if (error) {
-        console.error("Error connecting to email server:", error);
-    } else {
-        console.log("Email server is ready to send messages");
-    }
-});
-
+const { Resend } = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 // Function to send email
 const sendEmail = async (to, subject, text, html) => {
     try {
-        const info = await transporter.sendMail({
-            from: `"LedgerX" <${process.env.EMAIL_USER}>`,
+        const { data, error } = await resend.emails.send({
+            from: "LedgerX <onboarding@resend.dev>",
             to,
             subject,
             text,
             html,
         });
 
-        console.log("Message sent: %s", info.messageId);
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        if (error) {
+            console.error("Error sending email:", error);
+            return;
+        }
+
+        console.log("Message sent:", data.id);
 
     } catch (error) {
         console.error("Error sending email:", error);
